5881e27335b9fa3ccd3d8227f84dadff5429082a
bl-packages <bl.ilya.marozau@gmail.com>
2020-10-05 15:14:55 +0300

    chore(lerna): Lerna settings update

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,18 @@
+const {
+  utils: { getPackages },
+} = require('@commitlint/config-lerna-scopes');
+
 module.exports = {
   extends: [
     '@commitlint/config-conventional',
     '@commitlint/config-lerna-scopes',
   ],
   rules: {
+    'scope-enum': async (ctx) => {
+      const scopes = await getPackages(ctx);
+
+      return [2, 'always', [...scopes, 'lerna']];
+    },
     'subject-case': [0],
     'type-enum': [
       2,
